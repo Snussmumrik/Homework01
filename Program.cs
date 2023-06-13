d76150a7821921a7942c8997bd50c9995cfd94eb
Marina <PyatkovaM@yandex.ru>
2023-06-13 17:07:26 +0300

    добавили решение 2 задачи

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое целое число:  ");
+string userInput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userInput1);
+
+Console.Write("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userInput2);
+
+if (a > b)
+{
+    Console.WriteLine ($" {a} ");
+}
+else
+{
+    Console.WriteLine ($" {b} ");
+}
